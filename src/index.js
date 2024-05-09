36b1b126e4313ebd58f28c840b67cdadd4a4f3e9
Doha Kaddaf <doha_kaddaf@carrefour.com>
2024-05-09 21:29:14 +0200

    fix feedback

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
 const cards = [
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' },
-  { name: 'aquaman', img: 'aquaman.jpg' },
-  { name: 'batman', img: 'batman.jpg' },
-  { name: 'captain america', img: 'captain-america.jpg' },
-  { name: 'fantastic four', img: 'fantastic-four.jpg' },
-  { name: 'flash', img: 'flash.jpg' },
-  { name: 'green arrow', img: 'green-arrow.jpg' },
-  { name: 'green lantern', img: 'green-lantern.jpg' },
-  { name: 'ironman', img: 'ironman.jpg' },
-  { name: 'spiderman', img: 'spiderman.jpg' },
-  { name: 'superman', img: 'superman.jpg' },
-  { name: 'the avengers', img: 'the-avengers.jpg' },
-  { name: 'thor', img: 'thor.jpg' }
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
+  { name: "aquaman", img: "aquaman.jpg" },
+  { name: "batman", img: "batman.jpg" },
+  { name: "captain america", img: "captain-america.jpg" },
+  { name: "fantastic four", img: "fantastic-four.jpg" },
+  { name: "flash", img: "flash.jpg" },
+  { name: "green arrow", img: "green-arrow.jpg" },
+  { name: "green lantern", img: "green-lantern.jpg" },
+  { name: "ironman", img: "ironman.jpg" },
+  { name: "spiderman", img: "spiderman.jpg" },
+  { name: "superman", img: "superman.jpg" },
+  { name: "the avengers", img: "the-avengers.jpg" },
+  { name: "thor", img: "thor.jpg" },
 ];
 
 const memoryGame = new MemoryGame(cards);
 
 memoryGame.shuffleCards();
 
-window.addEventListener('load', (event) => {
-  let html = '';
+window.addEventListener("load", (event) => {
+  let html = "";
   memoryGame.cards.forEach((pic) => {
     html += `
       <div class="card" data-card-name="${pic.name}">
@@ -41,37 +41,41 @@ window.addEventListener('load', (event) => {
   });
 
   // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
+  document.querySelector("#memory-board").innerHTML = html;
 
   // Bind the click event of each element to a function
   let turnedCards = [];
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      card.classList.add('turned');
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", () => {
+      card.classList.add("turned");
       turnedCards.push(card);
       if (turnedCards.length < 2) {
         return;
       }
-      const card1Name = turnedCards[0].getAttribute('data-card-name');
-      const card2Name = turnedCards[1].getAttribute('data-card-name');
+      const card1Name = turnedCards[0].getAttribute("data-card-name");
+      const card2Name = turnedCards[1].getAttribute("data-card-name");
       if (memoryGame.checkIfPair(card1Name, card2Name)) {
         turnedCards.forEach((card) => {
-          card.classList.add('blocked');
+          card.classList.add("blocked");
         });
         turnedCards = [];
+        memoryGame.pairsClicked = [];
         if (memoryGame.checkIfFinished()) {
-          alert('You won!');
+          alert("You won!");
         }
       } else {
         setTimeout(() => {
           turnedCards.forEach((card) => {
-            card.classList.remove('turned');
+            card.classList.remove("turned");
           });
           turnedCards = [];
+          memoryGame.pairsClicked = [];
         }, 1000);
       }
-      document.querySelector('#pairs-clicked').innerText = memoryGame.pairsClicked;
-      document.querySelector('#pairs-guessed').innerText = memoryGame.pairsGuessed;
+      document.querySelector("#pairs-clicked").innerText =
+        memoryGame.pairsClicked;
+      document.querySelector("#pairs-guessed").innerText =
+        memoryGame.pairsGuessed;
     });
   });
 });
